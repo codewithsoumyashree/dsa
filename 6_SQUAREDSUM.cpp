591762026e205c9cb36611705ae17b8ed8e49ab8
Soumyashree Sahoo <121792834+codewithsoumyashree@users.noreply.github.com>
2023-01-29 23:52:53 +0530

    Add files via upload

---
+++ 6_SQUAREDSUM.cpp
@@ -0,0 +1,37 @@
+// Sum square difference
+// The sum of the squares of the first ten natural numbers is,
+// `1^2 + 2^2 + ... + 10^2 = 385`
+//
+// The square of the sum of the first ten natural numbers is,
+// `(1 + 2 + ... + 10)^2 = 55^2 = 3025`
+
+#include <iostream>
+
+int main()
+{
+  unsigned int tests;
+  std::cin >> tests;
+  while (tests--)
+  {
+    unsigned long long x;
+    std::cin >> x;
+
+    unsigned long long sum        = 0; // 1   + 2   + ...
+    unsigned long long sumSquared = 0; // 1^2 + 2^2 + ...
+
+    for (unsigned long long i = 1; i <= x; i++)
+    {
+      sum        += i;
+      sumSquared += i*i;
+    }
+    // chances are that your compiler (partially) unrolls this simple loop
+
+    // actually we don't need a loop for the sum (and the sum of squares)
+    // => see "Alternative" section above
+
+    // we had (1+2+...) instead of (1+2+...)^2
+    unsigned long long squaredSum = sum * sum;
+    std::cout << (squaredSum - sumSquared) << std::endl;
+  }
+  return 0;
+}
